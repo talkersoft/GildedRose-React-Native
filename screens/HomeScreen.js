ce3f348f41c41203ec26ff2a2827535c1369804e
Alker, Todd <talkersoft@gmail.com>
2019-01-29 21:38:07 -0500

    react native typescript shell

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-                My first React Native App!
+              REACT NATIVE ROCKS!
             </Text>
           </View>
 
